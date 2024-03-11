14d6afbd7c6e1b31896967a49387025b0e866557
Alvin Pratama <palveeen22@gmail.com>
2024-03-11 20:22:11 +0700

    feat: nav stack and tabs

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+//import screens here
+import 'package:learn_layout_flutter/screens/Product_Screen.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('To Second'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/Product_Screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:learn_layout_flutter/screens/Product_Details.dart';
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('To Third'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const TabBarDemo()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
